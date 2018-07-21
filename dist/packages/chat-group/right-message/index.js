8ce4abcd8fe6b8f8a1a4af644ca925e1c236c1f8
hocgin <hocgin@gmail.com>
2018-07-21 17:19:49 +0800

    up

---
+++ dist/packages/chat-group/right-message/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = Component({
+  data: {},
+  properties: {
+    msg: {
+      type: String,
+      value: 'This is a test msg!'
+    }
+  },
+  methods: {}
+});
